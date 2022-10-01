028dc1fde57826b68df8f0ecb90aa5206c46ee85
LazerLotus <luan.tsai1991@gmail.com>
2022-10-01 13:36:22 +0800

    add bootstrap js and css and popper js

--- app.js
+++ app.js
@@ -6,8 +6,21 @@ const port = 3000
 // require express-handlebars here
 const exphbs = require('express-handlebars')
 
+//require restautant list here
 const restaurantList = require('./restaurant.json')
+
+//setting static files
+app.use(express.static('public'))
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+//start and listen on the Express Server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
