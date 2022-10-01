47589a08615a510978cb2d6539dac2502232e644
LazerLotus <luan.tsai1991@gmail.com>
2022-10-01 12:27:06 +0800

    initiate the project

---
+++ app.js
@@ -0,0 +1,13 @@
+//require packages used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+
+// require express-handlebars here
+const exphbs = require('express-handlebars')
+
+const restaurantList = require('./restaurant.json')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
