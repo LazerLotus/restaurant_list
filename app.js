c050b9d46d9e52f129225732afe19dc7a4ed2b73
LazerLotus <luan.tsai1991@gmail.com>
2022-10-02 10:41:31 +0800

    add alert when user input empty string or no result

--- app.js
+++ app.js
@@ -29,7 +29,28 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
 //render result of search
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
-  res.render('index')
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+  })
+  console.log(restaurants.length)
+  console.log(!keyword.trim().length)
+
+  if (!keyword.trim().length) {
+    let alert = '<script>alert("請輸入有效字串！(不能僅含有空白字元)")</script>'
+    res.render('index', { restaurants: restaurantList.results, alert: alert })
+    return
+  }
+  console.log("a")
+  if (restaurants.length === 0) {
+    let alert = '<script> alert("' + `您輸入的關鍵字 ${keyword} 沒有找到結果` + '")</script>'
+    res.render('index', { restaurants: restaurantList.results, alert: alert })
+    console.log(restaurants)
+    console.log(alert)
+    return
+  }
+  console.log("b")
+  res.render('index', { restaurants: restaurants })
+  console.log("c")
 })
 
 //start and listen on the Express Server
